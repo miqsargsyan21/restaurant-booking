edffc7b3ff4f86a5d5c43e251c7d390411ea34ed
Michael Sargsyan <mikayel.sargsyan@mamble.co>
2023-07-02 21:10:33 +0400

    Added api docs

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import { connectToDatabase } from "./Modules/SequelizeModule.js";
+import swaggerUi from 'swagger-ui-express';
 import Routes from "./Routes/index.js";
 import bodyParser from "body-parser";
 import express from 'express';
 import dotenv from 'dotenv';
+import yaml from 'yamljs';
 
 let { PORT: port } = dotenv.config().parsed;
 port = port - 0 || 3000;
 
+const swaggerDocument = yaml.load('./swagger.yaml');
+
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
